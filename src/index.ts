2078a87311189d88b9a20e431698576c125c43ac
ashab20 <ashabuddin20@hotmail.com>
2023-06-10 09:34:36 +0600

    ts config and some route change

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 /*
 ? Title: 'PostgreSql Project' 
 * Description: 'mern stack ecommerce poroject'
@@ -7,50 +6,48 @@
 * Date: '2023-03-23'
 */
 
-
 import { config } from "dotenv";
 import routes from "./routes";
-import express from "express"
-import { PrismaClient } from "@prisma/client"
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use('/', routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-
+console.log("ok");
 
 config();
 
 // unhandle cought error
-process.on('uncaughtException', (err) =>{
-    console.log(`Error: ${err}`);
-    console.log(`Shutting down the server due to the uncounght server error`);
-    process.exit(1);
-    prisma.$disconnect();
-    // like undifine fundtion call
-})
-
-
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err}`);
+  console.log(`Shutting down the server due to the uncounght server error`);
+  prisma.$disconnect();
+  process.exit(1);
+  // like undifine fundtion call
+});
 
 // Module scufholding
 const port = process.env.APP_PORT;
 
-
+console.log(port);
 // Listening to servr
-const server = app.listen(port,()=>{
-    console.log(`Server listening to ${port}`)
-})
+const server = app.listen(port, () => {
+  console.log(`Server listening to ${port}`);
+});
 
-
-// unhadle Promise rejection 
-process.on('unhandledRejection', (err) => {
-    console.log(`Error: ${err}`);
-    console.log(`Server shutting down the server due to unhandle promise rejaction`);
-    server.close(()=>{
-        process.exit(1);
-    });
-    prisma.$disconnect();
-})
+// unhadle Promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err}`);
+  console.log(
+    `Server shutting down the server due to unhandle promise rejaction`
+  );
+  server.close(() => {
+      prisma.$disconnect();
+    process.exit(1);
+  });
+});
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import { indexTodo } from "@controller/userController"
+import express from "express";
+import { indexTodo } from "../controller/userController";
 
 const userRoutes = express.Router();
 
 // User routes
-userRoutes.get('/users',indexTodo);
+userRoutes.get("/users", indexTodo);
 
-userRoutes.get('/posts', (req, res) => {
+userRoutes.get("/posts", (req, res) => {
   // Handle user post listing logic
-  res.send('User Posts');
+  res.send("User Posts");
 });
 
 export { userRoutes };
